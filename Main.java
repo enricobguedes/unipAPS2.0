508781eaa1390b7c3535f48fec07311a8270dc55
enricobguedes <45374536+enricobguedes@users.noreply.github.com>
2021-11-18 20:43:33 -0300

    mainAPS
    yes

---
+++ Main.java
@@ -0,0 +1,60 @@
+package com.company;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+	// write your code her
+        Scanner nomeArquivo = new Scanner(System.in);
+        System.out.println("Entre com o nome do arquivo: (.txt)");
+        String arquivoComTxt = nomeArquivo.nextLine();
+
+        pesquisarArquivo(arquivoComTxt);
+
+
+
+    }
+
+    public static ArrayList<String> pesquisarArquivo(String arquivoComTxt) {
+        try {
+            ArrayList<String> dadosColetados = new ArrayList<>();
+
+            File documentoTxt = new File(arquivoComTxt);
+            Scanner leituraDeArquivo = new Scanner(documentoTxt);
+            int numeroDeLinhas = 0;
+            while (leituraDeArquivo.hasNextLine()) {
+                dadosColetados.add(leituraDeArquivo.nextLine());
+                numeroDeLinhas++;
+            }
+            leituraDeArquivo.close();
+
+            return classificarDados(dadosColetados);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Arquivo não encontrado");
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static ArrayList<String> classificarDados ( ArrayList<String> dadosColetados) {
+        int quantidadeDeEntradas = dadosColetados.size();
+        //Collections.sort(dadosColetados);
+        for (String i : dadosColetados) {
+            if (Objects.equals(i, "Deforestation")) {
+                dadosColetados.set(0, i);
+            }
+        }
+        System.out.println(dadosColetados);
+
+        return null;
+    }
+
+
+    // abrir arquivo txt
+    // transformar tudo em array list
+    // separar em data/nome/local
+    //
+    }
